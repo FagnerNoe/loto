8005e745b25f051d57665f4a4dcc77073747cdfb
Fagner Noe <fagnernezim@gmail.com>
2024-01-22 23:03:56 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -75,38 +75,6 @@ const apiUrl = "https://api.guidi.dev.br/loteria/lotofacil/ultimo";
 
                    }
 
-let installPrompt = "Instalar Aplicativo";
-const installButton = document.querySelector('#install');
-          
-
-   
-
-    window.addEventListener("beforeinstallprompt", (e) => {
-        e.preventDefault();
-        installPrompt = e.target;
-        installButton.removeAttribute('hidden');
-    });
-       
-    installButton.addEventListener("click", async () => {
-        if (!installPrompt){
-            return;
-        }
-        const result = await installPrompt.prompt();
-        console.log(`Install prompt was: ${result.outcome}`);
-        disableInAppInstallPrompt();
-    });
-
-    function disableInAppInstallPrompt() {
-        installPrompt = null;
-        installButton.setAttribute("hidden", "");
-    }
-
-    window.addEventListener("appinstalled", () => {
-        disableInAppInstallPrompt();
-    });
-
-   
-            
                                    
                 
                 
